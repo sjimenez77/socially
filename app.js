2d4bd6f42d8080b2b1870f3fe3edf5f1bb771f2b
Santos Jiménez Linares <sjimenez77@gmail.com>
2014-11-16 18:55:27 +0000

    Add and remove parties

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ if (Meteor.isClient) {
     
     angular.module("socially").controller("PartiesListCtrl", ['$scope', '$collection', function($scope, $collection){
         $collection(Parties).bind($scope, 'parties', true, true);
+        
+        $scope.remove = function(party){
+            $scope.parties.splice( $scope.parties.indexOf(party), 1 );
+        };
+        
     }]);
 }
 
